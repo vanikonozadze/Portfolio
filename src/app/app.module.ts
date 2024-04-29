8fa062bd896ddc0ad6969ee86ddba4a2b4b33bf1
Vaniko Nozadze <ivane.nozadze.1@btu.edu.ge>
2024-04-29 13:18:58 +0400

    Started to develop portfolio app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { IntroductionComponent } from './components/introduction/introduction.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    IntroductionComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,25 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent implements OnInit {
+  constructor(private router: Router, private elementRef: ElementRef) {}
+
+  ngOnInit(): void {}
+
+  aboutMeFunction(event: MouseEvent) {
+    const target = event.currentTarget as HTMLElement;
+    const sectionId = `${target.classList[0].slice(0, -2)}`;
+    const element = document.getElementById(sectionId) as HTMLElement;
+
+    console.log(target);
+
+    setTimeout(() => {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }, 100);
+  }
+}
