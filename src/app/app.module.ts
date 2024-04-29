8f56b83f70a7d499924bf5cb0501e652be68bd7a
Vaniko Nozadze <ivane.nozadze.1@btu.edu.ge>
2024-04-29 17:11:18 +0400

    Added about me and skills section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,19 +8,22 @@ import { MainComponent } from './components/main/main.component';
 import { IntroductionComponent } from './components/introduction/introduction.component';
 import { AboutMeComponent } from './components/about-me/about-me.component';
 
+// import function to register Swiper custom elements
+import { register } from 'swiper/element/bundle';
+// register Swiper custom elements
+register();
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainComponent,
     IntroductionComponent,
-    AboutMeComponent
+    AboutMeComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}
