56ce7fa32e4b6f01cb131796544f0ddbb7be7451
Vaniko Nozadze <ivane.nozadze.1@btu.edu.ge>
2024-05-05 15:11:20 +0400

    changed animation time and styling

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,4 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +6,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  constructor(private router: Router, private elementRef: ElementRef) {}
+  constructor() {}
 
   ngOnInit(): void {}
 
@@ -15,8 +14,6 @@ export class HeaderComponent implements OnInit {
     const target = event.currentTarget as HTMLElement;
     const sectionId = `${target.classList[0].slice(0, -2)}`;
     const element = document.getElementById(sectionId) as HTMLElement;
-
-    console.log(sectionId);
 
     setTimeout(() => {
       element.scrollIntoView({ behavior: 'smooth' });
